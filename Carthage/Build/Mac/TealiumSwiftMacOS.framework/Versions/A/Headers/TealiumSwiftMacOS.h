c1e8bb026b30c88963187a5cc2cd68943028bc4c
Merritt Tidwell <merritt.tidwell@gmail.com>
2016-11-08 12:19:39 -0800

    swifter visible

---
+++ Carthage/Build/Mac/TealiumSwiftMacOS.framework/Versions/A/Headers/TealiumSwiftMacOS.h
@@ -0,0 +1,19 @@
+//
+//  TealiumSwiftMacOS.h
+//  TealiumSwiftMacOS
+//
+//  Created by Merritt Tidwell on 11/8/16.
+//  Copyright © 2016 tealium. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TealiumSwiftMacOS.
+FOUNDATION_EXPORT double TealiumSwiftMacOSVersionNumber;
+
+//! Project version string for TealiumSwiftMacOS.
+FOUNDATION_EXPORT const unsigned char TealiumSwiftMacOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TealiumSwiftMacOS/PublicHeader.h>
+
+
